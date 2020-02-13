1ce8e5bf1108e5a27548e560612b1bd8baec5b93
James <james.waugh.r@gmail.com>
2020-02-12 22:54:23 -0500

    Update for new version of urfave/cli

--- main.go
+++ main.go
@@ -13,12 +13,12 @@ import (
 
 func makeKyoroOptions(cli *cli.Context) Options {
 	return Options{
-		DeckName:       cli.GlobalString("deck-name"),
-		ModelName:      cli.GlobalString("model-name"),
-		MaxSentences:   cli.GlobalInt("max-sentences"),
-		MonoligualMode: cli.GlobalBool("monolingual"),
-		InputPhrase:    cli.GlobalString("input"),
-		SilentMode:     cli.GlobalBool("silent"),
+		DeckName:       cli.String("deck-name"),
+		ModelName:      cli.String("model-name"),
+		MaxSentences:   cli.Int("max-sentences"),
+		MonoligualMode: cli.Bool("monolingual"),
+		InputPhrase:    cli.String("input"),
+		SilentMode:     cli.Bool("silent"),
 	}
 }
 
@@ -46,20 +46,25 @@ func runKyoro(options Options) bool {
 func main() {
 	app := cli.NewApp()
 	app.Name = "Kyoro, a Japanese sentence card generator"
-	app.Version = "0.5.0"
-	app.Author = "James Waugh"
+	app.Version = "0.6.0"
+	app.Authors = []*cli.Author{
+		&cli.Author{
+			Name:  "James Waugh",
+			Email: "james.waugh.r@gmail.com",
+		},
+	}
 	app.Description = `
 Kyoro builds bulk Japanese sentence cards faster by pulling sentences from online
 sources and importing them into Aki with sentence and vocabulary-focused modes.`
 	app.Flags = []cli.Flag{
-		cli.StringFlag{Name: "deck-name, d", Usage: "The Anki deck name to place generated cards in"},
-		cli.StringFlag{Name: "model-name, m", Usage: "The model to create cards with. *Required Fields*: japanese,english,reading,sentences"},
-		cli.StringFlag{Name: "input, i", Usage: "The Japanese phrase to create cards around. Leave blank to use the clipboard."},
-		cli.IntFlag{Name: "max-sentences, n", Value: 5, Usage: "Maximum number of example sentences to build"},
-		cli.BoolFlag{Name: "monolingual, 1", Usage: "Create Japanese-only cards with no English text"},
-		cli.BoolFlag{Name: "silent, s", Usage: "Don't create any confirmation dialogs and add cards unequivocally"},
+		&cli.StringFlag{Name: "deck-name, d", Usage: "The Anki deck name to place generated cards in"},
+		&cli.StringFlag{Name: "model-name, m", Usage: "The model to create cards with. *Required Fields*: japanese,english,reading,sentences"},
+		&cli.StringFlag{Name: "input, i", Usage: "The Japanese phrase to create cards around. Leave blank to use the clipboard."},
+		&cli.IntFlag{Name: "max-sentences, n", Value: 5, Usage: "Maximum number of example sentences to build"},
+		&cli.BoolFlag{Name: "monolingual, 1", Usage: "Create Japanese-only cards with no English text"},
+		&cli.BoolFlag{Name: "silent, s", Usage: "Don't create any confirmation dialogs and add cards unequivocally"},
 	}
-	app.Commands = []cli.Command{
+	app.Commands = []*cli.Command{
 		{
 			Name:  "sentences",
 			Usage: "Creates --max-sentences sentence cards, one sentence per card",
